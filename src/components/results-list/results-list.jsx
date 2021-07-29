6e1f73e7646156ffef3394611897ce339d2972af
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-29 16:02:41 +0300

    style for results-list

--- src/components/results-list/results-list.jsx
+++ src/components/results-list/results-list.jsx
@@ -4,7 +4,7 @@ function ResultsList() {
   return (
     <section className="app__section results-list">
       <div className="results-list__wrapper">
-        <h2 className="results-list">История конвертация</h2>
+        <h2 className="results-list__title">История конвертация</h2>
       </div>
       <ul className="results-list__list">
         {new Array(10).fill('').map((_, i) => <Result key={i}/>)}
